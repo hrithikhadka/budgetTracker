ae71ffc7b583c82b8c612f099db824c8ecdbcc77
Hritik Khadka <hritik.khadka@centria.fi>
2022-06-06 00:17:30 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import "./App.css";
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
+
+//initial values for expenses
+const initial_expenses = [
+  {
+    id: "e1",
+    title: "New Car",
+    amount: 300,
+    date: new Date(2021, 5, 16),
+  },
+  {
+    id: "e2",
+    title: "Gucci Boots",
+    amount: 200,
+    date: new Date(2021, 4, 13),
+  },
+  {
+    id: "e3",
+    title: "New House",
+    amount: 100,
+    date: new Date(2022, 8, 14),
+  },
+  {
+    id: "e4",
+    title: "Study Table",
+    amount: 150,
+    date: new Date(2019, 10, 15),
+  },
+
+  {
+    id: "e5",
+    title: "Laptop",
+    amount: 1500,
+    date: new Date(2019, 3, 15),
+  },
+
+  {
+    id: "e6",
+    title: "Water Bottle",
+    amount: 150,
+    date: new Date(2022, 2, 15),
+  },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(initial_expenses);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...expenses];
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Expenses Tracker</h1>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+
+import "./ExpenseForm.css";
+
+const ExpenseForm = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  //title change handler
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  //amount change handler
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  //date change handler
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  //form submit handler
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    props.onSaveExpenseData(expenseData);
+
+    //clearing all the input fields when form is submitted
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+            min="0.01"
+            step="0.01"
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            value={enteredDate}
+            onChange={dateChangeHandler}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
